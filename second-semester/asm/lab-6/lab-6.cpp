c9b23fd109cf1ea41e5871aa1bbc974b141443fb
Kononenko Daniil <neokv210@gmail.com>
2024-03-26 15:39:15 +0300

    Add folder for lab-6 and rename lab-1/test-3 to lab-1/task-3

---
+++ second-semester/asm/lab-6/lab-6.cpp
@@ -0,0 +1,48 @@
+// lab-6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	std::cout << "Input a, b for (ax + b = 0):" << std::endl;
+	std::cin >> a >> b;
+	if (a == 0 && b == 0) {
+		std::cout << "Infinity count of solutions" << std::endl;
+		return 0;
+	}
+	else if (a == 0 && b != 0) {
+		std::cout << "No solutions" << std::endl;
+		return 0;
+	}
+
+	int x{};
+	_asm {
+		mov eax, b
+		neg eax
+		cdq
+		idiv a
+		mov x, eax
+	}
+	std::cout << "Solution is " << x << std::endl;
+
+	return 0;
+}
