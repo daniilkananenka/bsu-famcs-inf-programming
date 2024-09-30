26fcc8c6e2455898a1c58cc90d02e891aac18885
Kononenko Daniil <neokv210@gmail.com>
2024-09-30 13:47:56 +0300

    Add GenericFirstTask

---
+++ third-semester/industrial-programming/lab-5/GenericFirstTask.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GenericFirstTask {
+  public static void main(String[] args) {
+    Integer[] arr = { 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15 };
+    Scanner scanner = new Scanner(System.in);
+
+    Integer userIn = scanner.nextInt();
+    System.out.println(has(arr, userIn) ? "Contains" : "Not contains");
+  }
+
+  public static <T> Boolean has(T[] arr, T value) {
+    return Arrays.asList(arr).contains(value);
+  }
+}
