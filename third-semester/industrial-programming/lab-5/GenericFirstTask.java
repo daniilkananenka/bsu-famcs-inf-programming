91dbd482e2262200187d7d7f926f137b5873a5ef
Kononenko Daniil <neokv210@gmail.com>
2024-09-30 14:08:11 +0300

    Fix scanner in generic tasks

--- third-semester/industrial-programming/lab-5/GenericFirstTask.java
+++ third-semester/industrial-programming/lab-5/GenericFirstTask.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class GenericFirstTask {
   public static void main(String[] args) {
     Integer[] arr = { 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15 };
-    Scanner scanner = new Scanner(System.in);
-
-    Integer userIn = scanner.nextInt();
-    System.out.println(has(arr, userIn) ? "Contains" : "Not contains");
+    try (Scanner scanner = new Scanner(System.in)) {
+      Integer userIn = scanner.nextInt();
+      System.out.println(has(arr, userIn) ? "Contains" : "Not contains");
+    }
   }
 
   public static <T> Boolean has(T[] arr, T value) {
--- third-semester/industrial-programming/lab-5/GenericForthTask.java
+++ third-semester/industrial-programming/lab-5/GenericForthTask.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class GenericForthTask {
   public static void main(String[] args) {
     Integer[] arr = { 1, 2, 3, 4, 5, 10, 11, 12, 10, 13, 14, 15 };
-    Scanner scanner = new Scanner(System.in);
-
-    Integer userIn = scanner.nextInt();
-    System.out.println("Count: " + count(arr, userIn));
+    try (Scanner scanner = new Scanner(System.in)) {
+      Integer userIn = scanner.nextInt();
+      System.out.println("Count: " + count(arr, userIn));
+    }
   }
 
   public static <T extends Comparable<T>> Integer count(T[] arr, T value) {
