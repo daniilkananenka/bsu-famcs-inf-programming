df34ffbffa781456732f5aa47ac64785db7f94da
Kononenko Daniil <neokv210@gmail.com>
2024-09-30 13:54:17 +0300

    Generic second task

---
+++ third-semester/industrial-programming/lab-5/GenericSecondTask.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GenericSecondTask {
+  public static void main(String[] args) {
+    Integer[] arr = { 1, 2, 3, 4, 5, 10, 11, 12, 10, 13, 14, 15 };
+    Scanner scanner = new Scanner(System.in);
+
+    Integer userIn = scanner.nextInt();
+    System.out.println("Count: " + count(arr, userIn));
+  }
+
+  public static <T> Integer count(T[] arr, T value) {
+    int counter = 0;
+    for (T el : arr) {
+      if (el == value)
+        counter++;
+    }
+    return counter;
+  }
+}
